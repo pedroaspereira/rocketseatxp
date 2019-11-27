f6cdcba5b86e65d4bf8e7444f27ace66e1f6aaca
Marcio Dias <marcio.dias@qsdobrasil.com>
2019-11-27 09:27:07 -0300

    metodo index Offer
    Index offer

--- server/src/app/controllers/OfferController.js
+++ server/src/app/controllers/OfferController.js
@@ -1,10 +1,16 @@
-import * as Yup from 'yup';
-import offer from '../schemas/Offer';
+import Offer from '../schemas/Offer';
 
 class OfferController {
+
+    async index(req, res) {
+        const offer = await Offer.find();
+
+        return res.json({ offer });
+    }
+
     async store(req, res) {
 
-        await offer.create({
+        await Offer.create({
             sponsor_id: req.body.sponsor_id,
             offer_description: req.body.offer_description,
             minimum_points: req.body.minimum_points,
@@ -12,32 +18,7 @@ class OfferController {
 
         return res.json({ ok: true });
     }
-    
-    /*
-    async update(req, res) {
-        const schema = Yup.object().shape({
 
-            sponsor_id: Yup.string(),
-            offer_description: Yup.string(),
-            minimum_points: Yup.number(),
-            
-        });
-
-        if (!(await schema.isValid(req.body))) {
-            return res.status(400).json({ error: 'Validation fails' });
-        }
-
-        const { sponsor_id, offer_description, minimum_points } = req.body;
-
-        const offer = await offer.findByPk(req.offerId);        
-
-        const data = await user.update(req.body);
-
-        return data 
-       
-
-    }
-    */
 }
 
 export default new OfferController();
--- server/src/routes.js
+++ server/src/routes.js
@@ -12,6 +12,7 @@ routes.post('/users', UserController.store);
 routes.put('/users', UserController.update);
 
 routes.post('/offer', OfferController.store);
+routes.get('/offer', OfferController.index);
 
 routes.post('/files', upload.single('file'), (req, res) => {
   return res.json({ ok: true });
